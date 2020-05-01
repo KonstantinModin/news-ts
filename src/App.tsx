fad4b288a6b79a731f87315d72951c247423af3b
KonstantinModin <konstantin.v.modin@gmail.com>
2020-05-01 07:35:39 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState({
+    data: [],
+    loading: false,
+    error: null
+  })
+
+  const url = 'https://newsapi.org/v2/top-headlines?q=trump&apiKey=9c84424f051843108bca2fea4726ae0c';
+  const config = {
+    method: 'GET'
+  };
+
+  const fetchData = async () => {
+    setState(state => ({ ...state, loading: true }));
+    try {
+      const res = await fetch(url, config);
+      const d = await res.json();
+      console.log(d);
+      setState(state => ({ ...state, loading: false, data: d }));
+
+    } catch (error) {
+      console.error('error');
+      setState(state => ({ ...state, loading: false, data: [], error }));
+
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
