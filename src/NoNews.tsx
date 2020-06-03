d84f7a3f33840c3a30873e391c77fa2c03b707eb
KonstantinModin <konstantin.v.modin@gmail.com>
2020-06-03 16:45:58 +0200

    noNewsComponent

---
+++ src/NoNews.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NoNews: React.FC = () => {
+    return (
+        <div className="message">
+            <h3>We are very sorry :( </h3>
+            <h1>No news were found on your search criteria</h1>
+            <h5>Wanna try something different ? </h5>
+        </div>
+    )
+}
+
+export default NoNews;
